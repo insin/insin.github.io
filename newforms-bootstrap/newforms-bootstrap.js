34705bb4c4cae480a104f91dcf55a5d7b5ac6874
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2015-01-05 20:19:17 +0000

    [newforms-bootstrap] v1.0.0

--- newforms-bootstrap/newforms-bootstrap.js
+++ newforms-bootstrap/newforms-bootstrap.js
@@ -1,5 +1,5 @@
-/**
- * newforms-bootstrap 0.1.0 (dev build at Mon, 01 Dec 2014 16:31:11 GMT) - https://github.com/insin/newforms-bootstrap
+/*!
+ * newforms-bootstrap 1.0.0 - https://github.com/insin/newforms-bootstrap
  * MIT Licensed
  */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.BootstrapForm=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -169,78 +169,86 @@ var BootstrapRadioInlineRenderer = RadioFieldRenderer.extend({
   }
 })
 
-var BootstrapForm = Form.extend({
-  spinner: SPINNER,
-
-  constructor: function BootstrapForm(kwargs) {
-    Form.call(this, kwargs)
-    // Fields have now been deep-cloned, so we can make any customisations
-    // necessary for Bootstrap without affecting other places the same Field
-    // instance may be used.
-    this._patchFields()
-  },
-
-  _patchFields:function() {
-    var fieldNames = Object.keys(this.fields)
-    for (var i = 0, l = fieldNames.length; i < l ; i++) {
-      var field = this.fields[fieldNames[i]]
-      if (field.widget instanceof CheckboxSelectMultiple) {
-        if (field.widget.renderer === CheckboxFieldRenderer) {
-          field.widget.renderer = BootstrapCheckboxRenderer
+var BootstrapForm = React.createClass({displayName: 'BootstrapForm',
+  statics: {
+    patchFields:function(form) {
+      if (form.__patchedByBootstrapForm) { return }
+      var fieldNames = Object.keys(form.fields)
+      for (var i = 0, l = fieldNames.length; i < l ; i++) {
+        var field = form.fields[fieldNames[i]]
+        if (field.widget instanceof CheckboxSelectMultiple) {
+          if (field.widget.renderer === CheckboxFieldRenderer) {
+            field.widget.renderer = BootstrapCheckboxRenderer
+          }
         }
-      }
-      else if (field.widget instanceof RadioSelect) {
-        if (field.widget.renderer === RadioFieldRenderer) {
-          field.widget.renderer = BootstrapRadioRenderer
+        else if (field.widget instanceof RadioSelect) {
+          if (field.widget.renderer === RadioFieldRenderer) {
+            field.widget.renderer = BootstrapRadioRenderer
+          }
         }
-      }
-      else if (field instanceof MultiValueField) {
-        if (field.fields.length < 5 &&
-            field.widget.formatOutput === MultiWidget.prototype.formatOutput) {
-          var colClass = 'col-md-' + (12 / field.fields.length)
-          field.widget.formatOutput = function(widgets) {
-            return React.createElement("div", {className: "row"}, 
-              widgets.map(function(widget)  {return React.createElement("div", {className: colClass}, widget);})
-            )
+        else if (field instanceof MultiValueField) {
+          if (field.fields.length < 5 &&
+              field.widget.formatOutput === MultiWidget.prototype.formatOutput) {
+            var colClass = 'col-sm-' + (12 / field.fields.length)
+            field.widget.formatOutput = function(widgets) {
+              return React.createElement("div", {className: "row"}, 
+                widgets.map(function(widget)  {return React.createElement("div", {className: colClass}, widget);})
+              )
+            }
           }
         }
       }
     }
   },
 
+  propTypes: {
+    form: React.PropTypes.instanceOf(Form).isRequired,
+    spinner: React.PropTypes.string
+  },
+
+  getDefaultProps:function() {
+    return {
+      spinner: SPINNER
+    }
+  },
+
   render:function() {
+    var form = this.props.form
+    if (!form.__patchedByBootstrapForm) {
+      BootstrapForm.patchFields(form)
+      form.__patchedByBootstrapForm = true
+    }
+    return React.createElement("div", null, 
+      this.renderRows()
+    )
+  },
+
+  renderRows:function() {
     var rows = []
-    var formErrors = this.nonFieldErrors()
+    var form = this.props.form
+    var formErrors = form.nonFieldErrors()
     if (formErrors.isPopulated()) {
-      rows.push(React.createElement("div", {key: this.addPrefix('__all__'), className: "alert alert-danger has-error"}, 
+      rows.push(React.createElement("div", {key: form.addPrefix('__all__'), className: "alert alert-danger has-error"}, 
         formErrors.messages().map(errorMessage)
       ))
     }
-    rows.push.apply(rows, this.visibleFields().map(function(field) 
-      {return React.createElement(BootstrapField, {key: field.htmlName, field: field, spinner: this.spinner || SPINNER});}.bind(this)
+    rows.push.apply(rows, form.visibleFields().map(function(field) 
+      {return React.createElement(BootstrapField, {key: field.htmlName, field: field, spinner: this.props.spinner});}.bind(this)
     ))
-    var hiddenFields = this.hiddenFields()
+    var hiddenFields = form.hiddenFields()
     if (hiddenFields.length > 0) {
-      rows.push(React.createElement("div", {key: this.addPrefix('__hiddenFields__'), style: {display: 'none'}}, 
+      rows.push(React.createElement("div", {key: form.addPrefix('__hiddenFields__'), style: {display: 'none'}}, 
         hiddenFields.map(function(field)  {return field.render();})
       ))
     }
-    if (this.nonFieldPending()) {
-      rows.push(React.createElement("span", {key: this.addPrefix('__pending__'), className: "help-block"}, 
-        React.createElement("img", {src: this.spinner || SPINNER}), " Validating…"
+    if (form.nonFieldPending()) {
+      rows.push(React.createElement("span", {key: form.addPrefix('__pending__'), className: "help-block"}, 
+        React.createElement("img", {src: this.props.spinner}), " Validating…"
       ))
     }
     return rows
   }
 })
-
-BootstrapForm.render = function(form) {
-  if (!form.__patchedByBootstrapForm) {
-    BootstrapForm.prototype._patchFields.call(form)
-    form.__patchedByBootstrapForm = true
-  }
-  return BootstrapForm.prototype.render.call(form)
-}
 
 extend(BootstrapForm, {
   CheckboxRenderer: BootstrapCheckboxRenderer
